9fc470c01e22b5bce7d7c494ae104428852dc3f1
Nathan | Z3T4 <33010830+ZKSuaVitel@users.noreply.github.com>
2019-11-24 17:47:40 -0600

    Newa Feed

--- app/src/main/java/dev/z3t4/doki/MainActivity.java
+++ app/src/main/java/dev/z3t4/doki/MainActivity.java
@@ -3,11 +3,14 @@ package dev.z3t4.doki;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 
+import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import butterknife.BindView;
@@ -20,6 +23,8 @@ import dev.z3t4.doki.ui.NewsFragment;
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.bottom_navigation_view) BottomNavigationView bottomNavigationView;
+    @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.app_bar_layout) AppBarLayout appBarLayout;
 
     private AnimeFragment animeFragment = new AnimeFragment();
     private MangaFragment mangaFragment = new MangaFragment();
@@ -31,11 +36,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (savedInstanceState == null){
+            changeFragment(animeFragment, AnimeFragment.class.getSimpleName());
+        }
+
         ButterKnife.bind(this);
-
-        changeFragment(animeFragment, AnimeFragment.class.getSimpleName());
         setupBottomNavigationView();
+        setupAppbarLayout();
 
+    }
+
+    private void setupAppbarLayout() {
+    setSupportActionBar(toolbar);
     }
 
 
@@ -46,21 +58,34 @@ public class MainActivity extends AppCompatActivity {
 
                 case R.id.anime:
                     changeFragment(animeFragment, AnimeFragment.class.getSimpleName());
+                    changeAppBarBackground(false);
                     break;
                 case R.id.manga:
                     changeFragment(mangaFragment, MangaFragment.class.getSimpleName());
+                    changeAppBarBackground(false);
                     break;
                 case R.id.news:
                     changeFragment(newsFragment, NewsFragment.class.getSimpleName());
+                    changeAppBarBackground(true);
                     break;
                 case R.id.library:
                     changeFragment(libraryFragment, LibraryFragment.class.getSimpleName());
+                    changeAppBarBackground(false);
                     break;
 
             }
 
             return true;
         });
+    }
+
+    private void changeAppBarBackground(boolean b) {
+    if (b){
+        appBarLayout.setBackgroundResource(R.drawable.tab_divider);
+    } else {
+        appBarLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.appbar_bottom_tab_color));
+    }
+
     }
 
     public void changeFragment(Fragment fragment, String tagFragmentName) {
--- app/src/main/java/dev/z3t4/doki/ui/NewsFragment.java
+++ app/src/main/java/dev/z3t4/doki/ui/NewsFragment.java
@@ -9,15 +9,28 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import dev.z3t4.doki.News.Feed;
+import dev.z3t4.doki.ParallaxEffect.ParallaxRecyclerView;
 import dev.z3t4.doki.R;
 
 public class NewsFragment extends Fragment {
+
+    @BindView(R.id.recyclerview)
+    ParallaxRecyclerView recyclerView;
 
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.host_news, container, false);
+
+        ButterKnife.bind(this, view);
+
+        Feed feed = new Feed(getActivity(), recyclerView );
+        feed.execute();
+
         return view;
     }
 }
