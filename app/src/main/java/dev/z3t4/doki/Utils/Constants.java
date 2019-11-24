dcd7751b1a4aa72efa7a102d19db9860d16fba63
Nathan | Z3T4 <33010830+ZKSuaVitel@users.noreply.github.com>
2019-11-24 16:05:19 -0600

    For resources & links

---
+++ app/src/main/java/dev/z3t4/doki/Utils/Constants.java
@@ -0,0 +1,12 @@
+package dev.z3t4.doki.Utils;
+
+import dev.z3t4.doki.R;
+
+public class Constants {
+
+    public static String broadcast = GenericContext.getContext().getResources().getString(R.string.broadcast);
+    public static String history = GenericContext.getContext().getResources().getString(R.string.history);
+    public static String calendar = GenericContext.getContext().getResources().getString(R.string.calendar);
+    public static String directory = GenericContext.getContext().getResources().getString(R.string.directory);
+
+}
